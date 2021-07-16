19c4bf9e09fc96dad22197fe9236408f2aded260
cleitonputtlitz <cleitonputtlitz@gmail.com>
2021-07-15 21:15:31 -0300

    transição entre telas e gerência de estados

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,23 @@ class BytebankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: FormularioTransferencia(),
-        //ListaTransferencias(),
+        body: ListaTransferencias(),
       ),
     );
   }
 }
 
-class FormularioTransferencia extends StatelessWidget {
-  const FormularioTransferencia({Key? key}) : super(key: key);
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  _FormularioTransferenciaState createState() =>
+      _FormularioTransferenciaState();
+}
+
+class _FormularioTransferenciaState extends State<FormularioTransferencia> {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+
+  final TextEditingController _controladorCampoValor = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -27,58 +35,81 @@ class FormularioTransferencia extends StatelessWidget {
       appBar: AppBar(
         title: Text("Criando transferência"),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-                style: TextStyle(fontSize: 24.0),
-                decoration: InputDecoration(
-                  labelText: 'Número da conta',
-                  hintText: '0000',
-                ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-                style: TextStyle(fontSize: 24.0),
-                decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-              labelText: 'Valor',
-              hintText: '0.00',
-            ),
-            keyboardType: TextInputType.number,
-            ),
-          ),
-          ElevatedButton(onPressed: () {}, child: Text("Confirmar")),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Editor(
+                controlador: _controladorCampoNumeroConta,
+                rotulo: 'Número da conta',
+                dica: '0000'),
+            Editor(
+                controlador: _controladorCampoValor,
+                rotulo: 'Valor',
+                dica: '0.00',
+                icone: Icons.monetization_on),
+            ElevatedButton(
+                onPressed: () => _criaTransferencia(context),
+                child: Text("Confirmar")),
+          ],
+        ),
       ),
     );
   }
+
+  void _criaTransferencia(BuildContext context) {
+    final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor = double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      print("Realizando a transferencia: ${transferenciaCriada.toString()}");
+      Navigator.pop(context, transferenciaCriada);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          //Apresenta conteúdo em uma barra na parte inferior da tela
+          SnackBar(content: Text("Não foi possível realizar a transferência")));
+    }
+  }
 }
 
-class ListaTransferencias extends StatelessWidget {
+class ListaTransferencias extends StatefulWidget {
+  final List<Transferencia> _transferencias = [];
+
+  @override
+  _ListaTransferenciasState createState() => _ListaTransferenciasState();
+}
+
+class _ListaTransferenciasState extends State<ListaTransferencias> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Transferências"),
       ),
-      body: Column(
-        children: [
-          ItemTransferencia(Transferencia(100.00, 123456)),
-          ItemTransferencia(Transferencia(200.00, 147852)),
-          ItemTransferencia(Transferencia(300.00, 25896)),
-          ItemTransferencia(Transferencia(400.00, 96321)),
-          ItemTransferencia(Transferencia(500.00, 96321)),
-        ],
+      body: ListView.builder(
+        itemCount: widget._transferencias.length,
+        itemBuilder: (context, indice) {
+          final transferencia = widget._transferencias[indice];
+          print("carregando transf $transferencia");
+          return ItemTransferencia(transferencia);
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          print("clicou no botão");
+          final Future future =
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return FormularioTransferencia();
+          }));
+          future.then((transferenciaRecebida) {
+            print('recebeu a transf. ${transferenciaRecebida.toString()}');
+            if (transferenciaRecebida != null) {
+              print(
+                  "widget._transferencias.length ${widget._transferencias.length}");
+              setState(() {
+                widget._transferencias.add(transferenciaRecebida);
+              });
+            }
+          });
         },
       ),
     );
@@ -106,5 +137,40 @@ class Transferencia {
   final double valor;
   final int numeroConta;
 
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
+
   Transferencia(this.valor, this.numeroConta);
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData? icone;
+
+  Editor(
+      {required this.controlador,
+      required this.rotulo,
+      required this.dica,
+      this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(fontSize: 24.0),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
 }
